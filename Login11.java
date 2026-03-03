9062c2640142f5c79a346a95c4770f2233111f8d
Ashok Pusunuru <pusunuruashok99@gmail.com>
2026-03-03 16:25:50 -0500

    the details is not been in system

---
+++ Login11.java
@@ -0,0 +1,13 @@
+public class Login11   {
+
+    
+    public static void main(String[] args) {
+        
+
+        System.out.println("the details has been hidden in the system properly");
+        System.out.println("the details has been not hidden in the system properly");
+        System.out.println("the matching details in the system has been hidden and not proper to make it hiiden in the system");
+        System.out.println("the details has been hidden in the system properly and not proper to make it hiiden in the system");
+    }
+
+}
---
+++ Login22.java
@@ -0,0 +1,12 @@
+public class Login22{
+
+    public static void main(String[] args) {
+        
+
+        System.out.println("the details has been hidden in the system properly");
+        System.out.println("the details has been not hidden in the system properly");
+        System.out.println("the matching details in the system has been hidden and not proper to make it hiiden in the system");
+        System.out.println("the details has been hidden in the system properly and not proper to make it hiiden in the system");
+    }
+}
+
